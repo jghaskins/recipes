2712ff4892c0ed3e265df7b0f33af3fc72178447
Joe Haskins <joe@commercialprogression.com>
2013-03-26 08:50:42 -0400

    added apache 2.4 install cookbook

---
+++ cookbooks/custom/recipes/install_apache_2.4.rb
@@ -0,0 +1,27 @@
+#
+# Author::  Joe Haskins(<joe@commercialprogression.com.com>)
+# Cookbook Name:: install_apache2.4
+# Recipe:: default
+#
+
+case node['platform']
+when 'centos', 'redhat', 'fedora'
+  # Configure repo
+  yum_repository "apache2.4" do
+    url "https://s3.amazonaws.com/apache2.4/repo/CentOS/6/$basearch"
+    action :add
+  end
+end
+
+pkgs = value_for_platform(
+  %w{ centos redhat fedora } => {
+    "default" => %w{ httpd }
+  },
+  "default" => %w{ httpd }
+)
+
+pkgs.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
