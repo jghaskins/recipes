5c3c52ae6efd4803f8a4588abdb6e0a8b15d083e
jghaskins <joseph.haskins@gmail.com>
2013-03-31 19:18:36 -0300

    Update apache2.4_add_repo.rb

--- cookbooks/custom/recipes/apache2.4_add_repo.rb
+++ cookbooks/custom/recipes/apache2.4_add_repo.rb
@@ -4,11 +4,16 @@
 #
 remote_file "/etc/yum.repos.d/apache_2-4.repo" do
   source "https://s3.amazonaws.com/apache2.4/repo/apache_2-4.repo"
-  notifies :run, resources(:execute => "yum_cache"), :immediately
+  notifies :run, resources(:execute => "yum_clear_cache"), :immediately
+  notifies :run, resources(:execute => "yum_rebuild_cache"), :immediately
 end
 
-execute "yum_cache" do
+execute "yum_clear_cache" do
   command "yum clear all"
+  action :nothing
+end
+
+execute "yum_rebuild_cache" do
   command "yum makecache"
   action :nothing
 end
