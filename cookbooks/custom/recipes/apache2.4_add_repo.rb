f64413b30a141b92c01d66968e9a4723a030d985
Joe Haskins <joe@commercialprogression.com>
2013-03-31 12:39:54 -0400

    removed yum recipes & added cache clear to apache repo add

--- cookbooks/custom/recipes/apache2.4_add_repo.rb
+++ cookbooks/custom/recipes/apache2.4_add_repo.rb
@@ -4,10 +4,11 @@
 #
 remote_file "/etc/yum.repos.d/apache_2-4.repo" do
   source "https://s3.amazonaws.com/apache2.4/repo/apache_2-4.repo"
-  notifies :run, resources(:execute => "yum_makecache"), :immediately
+  notifies :run, resources(:execute => "yum_cache"), :immediately
 end
 
-execute "yum_makecache" do
+execute "yum_cache" do
+  command "yum clear all"
   command "yum makecache"
   action :nothing
 end
